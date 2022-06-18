212c242b9c71318cbd482075f238c5bc9b4c3d58
nhuytan <42280131+nhuytan@users.noreply.github.com>
2022-06-18 10:27:40 -0500

    Update MyResource.java

--- src/main/java/turnConTest/com/turn/MyResource.java
+++ src/main/java/turnConTest/com/turn/MyResource.java
@@ -338,29 +338,7 @@ public class MyResource {
 		}
 	}
 	
-	
-	public boolean isValidName(String name)
-{
-    String specialCharacters=" !#$%&'()*+,-./:;<=>?@[]^_`{|}~0123456789";
-    String str2[]=name.split("");
-    int count=0;
-    for (int i=0;i<str2.length;i++)
-    {
-        if (specialCharacters.contains(str2[i]))
-        {
-            count++;
-        }
-    }       
 
-    if (name!=null && count==0 )
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 	// http://localhost:8080/com.turn/api/addUser/abff
 	@GET
 	@Path("/addUser/{userName}/{pass}")
@@ -371,7 +349,7 @@ public class MyResource {
 		int checkL = checkLogin(token);
 		if (checkL == 1) {
 			if (EmployeeDAO.isSameName(userName)) {
-			#if (isValidName(userName)) {
+			//if (isValidName(userName)) {
 				return "{\"error\": \"sameName\"}";
 			}
 			employee = EmployeeDAO.getEmployee();
